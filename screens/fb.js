8aafacfc51ce3b037f78cba81c5ceb335b1620f3
Samarth <misrarashi30@gmail.com>
2021-09-02 08:11:10 +0530

    hi

---
+++ screens/fb.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text} from 'react-native'
+
+export default class FbScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Facebook</Text>
+            </View>
+        )
+    }
+}
